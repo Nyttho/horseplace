87ca80627e17b63937bf79cbad75ca09800f552f
Anthony DUMAS <a.dumas@baclesse.unicancer.fr>
2025-02-24 15:09:14 +0100

    offer component

---
+++ app/components/Offer.jsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import Image from "next/image";
+
+export default function Offer({ offer }) {
+  return (
+    <div className="flex-1 flex flex-col items-center justify-center border-[1px] border-primary-dark py-8 rounded-sm">
+      <div className="w-16 h-16 bg-primary-dark rounded-full my-5 overflow-hidden">
+        <Image src={offer.img} alt={offer.title} width={500} height={500} />
+      </div>
+      <div className="text-primary-dark mb-10 text-center">
+        <h3 className="text-lg">{offer.title}</h3>
+        <h4>{offer.subtitle}</h4>
+      </div>
+      <div>
+        <ul className="space-y-4 text-black/50">
+          <li>{offer.feature1}</li>
+          <li>{offer.feature2}</li>
+          <li>{offer.feature3}</li>
+        </ul>
+      </div>
+      <div className="mt-16">
+        <Link href="#">
+          <div className="border-[1px] border-primary-dark py-2 w-40 text-primary-dark rounded-md text-center">Start Now</div>
+        </Link>
+      </div>
+    </div>
+  );
+}
--- app/tarif/page.jsx
+++ app/tarif/page.jsx
@@ -1,5 +1,50 @@
-const Tarif = () => {
-  return <h1>Tarif</h1>;
+import Offer from "../components/Offer";
+import avatar from "@/public/static/images/avatar.jpeg"
+
+export default function Tarif (){
+  const fakeOffers = [
+    {
+      id: 1,
+      title: "Title",
+      subtitle: "Subtitle",
+      img: avatar,
+      feature1: "Feature1",
+      feature2: "Feature2",
+      feature3: "Feature3"
+
+    },
+    {
+      id: 2,
+      title: "Title",
+      subtitle: "Subtitle",
+      img: avatar,
+      feature1: "Feature1",
+      feature2: "Feature2",
+      feature3: "Feature3"
+
+    },
+    {
+      id: 3,
+      title: "Title",
+      subtitle: "Subtitle",
+      img: avatar,
+      feature1: "Feature1",
+      feature2: "Feature2",
+      feature3: "Feature3"
+
+    },
+  ]
+  return( 
+  <section>
+    <h1>Tarif</h1>
+      <div className="flex gap-14 w-3/5 mx-auto">
+        {fakeOffers.map(fake => (
+          <Offer key={fake.id} offer={fake}/>
+        ))}
+      </div>
+  </section>
+
+);
 };
 
-export default Tarif;
+
