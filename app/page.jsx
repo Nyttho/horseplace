ff1a867ef48d6c1a5587b29e1e4e4f9a6271a21b
Anthony DUMAS <a.dumas@baclesse.unicancer.fr>
2025-02-24 14:27:34 +0100

    accueil ok

--- app/page.jsx
+++ app/page.jsx
@@ -106,6 +106,19 @@ export default function Home() {
       </section>
       <section>
         <div className="relative w-full h-80 overflow-hidden">
+          <div className="absolute inset-0 bg-gray-800/25 z-10"></div>
+          <div className="absolute top-1/2 left-40 transform -translate-y-1/2 z-20 text-white space-y-3">
+            <p className="font-bold text-4xl">Prêt à rejoindre la révolution équestre ?</p>
+            <p className="text-3xl">Demandez votre devis gratuit.</p>
+          </div>
+          <div className="absolute z-20 top-1/2 right-48 transform -translate-y-1/2 flex flex-col gap-3">
+            <Link href="/devis">
+              <div className="bg-white text-primary-dark py-1 rounded-md text-center font-bold w-52  px-10">Demander un devis</div>
+            </Link>
+            <Link href="/contact">
+              <div className="bg-transparent text-white p-3 rounded-md border-[3px] border-white text-center">Nous contacter</div>
+            </Link>
+          </div>
           <Image src={horse} fill className="object-cover object-[0%_35%]"  alt="image of a horse "/>
         </div>
       </section>
