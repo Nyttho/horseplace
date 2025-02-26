9681f9229f62f0d1ec2644bb0dbe34b9aaa52de0
Anthony DUMAS <a.dumas@baclesse.unicancer.fr>
2025-02-26 13:39:22 +0100

    contact page

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,5 +1,21 @@
+import ContactForm from "../components/ContactForm";
+
 const Contact = () => {
-  return <h1>contact</h1>;
+
+
+  return( 
+  <>
+   <section className="bg-secondary-light py-10 px-20 ">
+     <h2 className="text-6xl text-primary-dark text-center my-10">Nous contacter</h2>
+    <ContactForm />
+   </section>
+
+
+
+
+
+  </>
+);
 };
 
 export default Contact;
