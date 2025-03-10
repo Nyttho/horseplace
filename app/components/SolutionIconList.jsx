209acd62fa9df5d6b754c0470f451bc32309b59d
Anthony DUMAS <a.dumas@baclesse.unicancer.fr>
2025-03-10 15:27:37 +0100

    factorisation what is section

---
+++ app/components/SolutionIconList.jsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+
+export default function SolutionIconList({ icon, label, text }) {
+  return (
+    <li className="flex gap-6 items-center max-w-2xl">
+      <div className="w-20 h-20 flex-shrink-0">
+        <Image
+          src={icon}
+          width={96}
+          height={96}
+          alt=""
+          className="object-contain object-center"
+        />
+      </div>
+      <div>
+        <h4 className="font-bold text-xl">{label} :</h4>
+        <p>{text}</p>
+      </div>
+    </li>
+  );
+}
--- app/solution/page.jsx
+++ app/solution/page.jsx
@@ -5,29 +5,42 @@ import horsePicto from "@/public/static/images/horse-picto.png";
 import heartPicto from "@/public/static/images/heart-picto.png";
 import instructionPicto from "@/public/static/images/instructions-picto.png";
 import headPicto from "@/public/static/images/head-picto.png";
-import eInk from "@/public/static/images/e-ink_illustration.png"
-import hero from "@/public/static/images/illustration_horse_2.jpg"
-import SolutionCheck from "@/app/components/SolutionCheck"
+import eInk from "@/public/static/images/e-ink_illustration.png";
+import hero from "@/public/static/images/illustration_horse_2.jpg";
+import SolutionCheck from "@/app/components/SolutionCheck";
+import SolutionIconList from "@/app/components/SolutionIconList";
 const Solution = () => {
   return (
     <>
-    {/* hero solution section */}
-    <section className="h-screen">
-      <div className="relative w-full h-full overflow-hidden">
-      <div className="absolute inset-0 bg-gray-800/25 z-10"></div>
-      <div className="text-white absolute top-20 left-1/2 transform -translate-x-1/2 flex flex-col items-center z-10">
-        <h2 className="text-5xl text-center font-bold">La <span className="bg-primary-dark px-1">digitalisation</span> de votre écurie, enfin <span className="underline">abordable</span></h2>
-        <p className="text-lg text-center w-3/4 mt-12">Offrez à votre écurie une gestion moderne et abordable grâce à nos écrans e-Ink robustes et personnalisables. La digitalisation ,n'a jamais été aussi accessible !</p>
-        <Link href="/tarif">
+      {/* hero solution section */}
+      <section className="h-screen">
+        <div className="relative w-full h-full overflow-hidden">
+          <div className="absolute inset-0 bg-gray-800/25 z-10"></div>
+          <div className="text-white absolute top-20 left-1/2 transform -translate-x-1/2 flex flex-col items-center z-10">
+            <h2 className="text-5xl text-center font-bold">
+              La <span className="bg-primary-dark px-1">digitalisation</span> de
+              votre écurie, enfin <span className="underline">abordable</span>
+            </h2>
+            <p className="text-lg text-center w-3/4 mt-12">
+              Offrez à votre écurie une gestion moderne et abordable grâce à nos
+              écrans e-Ink robustes et personnalisables. La digitalisation ,n'a
+              jamais été aussi accessible !
+            </p>
+            <Link href="/tarif">
               <div className="bg-primary-dark text-white rounded-md mt-10 text-center w-60 py-5 font-bold">
                 En savoir plus
               </div>
             </Link>
-      </div>
-        <Image src={hero}  alt="cheval au galop" fill={true} className="object-cover object-top"/>
-      </div>
-    </section>
-    {/* qu'est ce que section */}
+          </div>
+          <Image
+            src={hero}
+            alt="cheval au galop"
+            fill={true}
+            className="object-cover object-top"
+          />
+        </div>
+      </section>
+      {/* qu'est ce que section */}
       <section className="py-14">
         <div className="flex">
           <div className="flex-1 flex flex-col justify-between items-start w-1/2 ml-40 my-28">
@@ -46,99 +59,40 @@ const Solution = () => {
           </div>
           <div className="bg-primary-dark p-14 w-full rounded-l-2xl flex-1">
             <ul className="text-white flex flex-col content-between w-full gap-5">
-              <li className="flex gap-6 items-center max-w-2xl">
-                <div className="w-20 h-20 flex-shrink-0">
-                  <Image
-                    src={horsePicto}
-                    width={96}
-                    height={96}
-                    alt=""
-                    className="object-contain object-center"
-                  />
-                </div>
-                <div>
-                  <h4 className="font-bold text-xl">
-                    Suivi des caractéristiques du cheval :
-                  </h4>
-                  <p>
-                    Nom, âge, alimentation spécifique, comportements à
-                    surveiller... Tout est visible en un coup d'œil.
-                  </p>
-                </div>
-              </li>
-              <li className="flex gap-6 items-center max-w-2xl">
-                <div className="w-20 h-20 flex-shrink-0">
-                  <Image
-                    src={heartPicto}
-                    width={96}
-                    height={96}
-                    alt=""
-                    className="object-contain object-center"
-                  />
-                </div>
-                <div>
-                  <h4 className="font-bold text-xl">
-                    Affichage des soins et traitements :
-                  </h4>
-                  <p>
-                    Traitements en cours, prochaine visite du vétérinaire,
-                    période de repos...Le planning est toujours à jour, sans
-                    erreur.
-                  </p>
-                </div>
-              </li>
-              <li className="flex gap-6 items-center max-w-2xl">
-                <div className="w-20 h-20 flex-shrink-0">
-                  <Image
-                    src={instructionPicto}
-                    width={96}
-                    height={96}
-                    alt=""
-                    className="object-contain object-center"
-                  />
-                </div>
-                <div>
-                  <h4 className="font-bold text-xl">
-                    Instructions personnalisées pour chaque pensionnaire :
-                  </h4>
-                  <p>
-                    Consignes précises pour le palefrenier, informations pour
-                    l'équipe de soin ou les propriétaires.
-                  </p>
-                </div>
-              </li>
-              <li className="flex gap-6 items-center max-w-2xl">
-                <div className="w-20 h-20 flex-shrink-0">
-                  <Image
-                    src={headPicto}
-                    width={96}
-                    height={96}
-                    alt=""
-                    className="object-contain object-center"
-                  />
-                </div>
-                <div>
-                  <h4 className="font-bold text-xl">
-                    Fini les oublis et les confusions :
-                  </h4>
-                  <p>
-                    Tout le monde accède aux informations au bon moment,
-                    directement sur place.
-                  </p>
-                </div>
-              </li>
+              <SolutionIconList
+                icon={horsePicto}
+                label="Suivi des caractéristiques du cheval"
+                text="Nom, âge, alimentation spécifique, comportements à surveiller... Tout est visible en un coup d'œil."
+              />
+              <SolutionIconList
+                icon={heartPicto}
+                label="Affichage des soins et traitements"
+                text="Traitements en cours, prochaine visite du vétérinaire, période de repos...Le planning est toujours à jour, sans erreur."
+              />
+              <SolutionIconList
+                icon={instructionPicto}
+                label="Instructions personnalisées pour chaque pensionnaire"
+                text="Consignes précises pour le palefrenier, informations pour l'équipe de soin ou les propriétaires."
+              />
+              <SolutionIconList
+                icon={headPicto}
+                label="Fini les oublis et les confusions"
+                text="Tout le monde accède aux informations au bon moment, directement sur place."
+              />
             </ul>
           </div>
         </div>
       </section>
 
-{/* pourquoi section */}
+      {/* pourquoi section */}
       <section className="bg-secondary-light py-14">
-      <div className="mx-40">
-          <h3 className="text-2xl font-bold mb-10">Pourquoi choisir Horsplace ?</h3>
+        <div className="mx-40">
+          <h3 className="text-2xl font-bold mb-10">
+            Pourquoi choisir Horsplace ?
+          </h3>
           <p className="text-primary-dark text-xl mb-10">
-            Horsplace est actuellement la seule solution sur le marché capable de
-            réunir toutes les fonctionnalités essentielles pour une gestion
+            Horsplace est actuellement la seule solution sur le marché capable
+            de réunir toutes les fonctionnalités essentielles pour une gestion
             optimale des écuries :
           </p>
           <div className="flex items-end">
@@ -152,11 +106,17 @@ const Solution = () => {
             <div className="bg-white grow p-10 rounded-3xl h-[420px] relative overflow-hidden">
               <h4 className="text-center text-xl font-bold">Ecran e-ink</h4>
               <div className="w-[520px] h-full absolute left-1/2 transform -translate-x-1/2 top-28">
-                <Image src={eInk} width={500} height={500} alt="illustration de l'écran e-ink" className="object-cover saturate-[1.60]"/>
+                <Image
+                  src={eInk}
+                  width={500}
+                  height={500}
+                  alt="illustration de l'écran e-ink"
+                  className="object-cover saturate-[1.60]"
+                />
               </div>
             </div>
           </div>
-      </div>
+        </div>
       </section>
       <section>
         <Estimation />
