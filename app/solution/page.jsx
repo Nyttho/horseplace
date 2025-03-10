d75c581c6ad34e3e9fe20542600cb745395c49f4
Anthony DUMAS <a.dumas@baclesse.unicancer.fr>
2025-03-10 13:48:56 +0100

    hero solution

--- app/solution/page.jsx
+++ app/solution/page.jsx
@@ -7,9 +7,26 @@ import instructionPicto from "@/public/static/images/instructions-picto.png";
 import headPicto from "@/public/static/images/head-picto.png";
 import checkIcon from "@/public/static/images/check-icon.png";
 import eInk from "@/public/static/images/e-ink_illustration.png"
+import hero from "@/public/static/images/illustration_horse_2.jpg"
 const Solution = () => {
   return (
     <>
+    {/* hero solution section */}
+    <section className="h-screen">
+      <div className="relative w-full h-full overflow-hidden">
+      <div className="absolute inset-0 bg-gray-800/25 z-10"></div>
+      <div className="text-white absolute top-20 left-1/2 transform -translate-x-1/2 flex flex-col items-center z-10">
+        <h2 className="text-5xl text-center font-bold">La <span className="bg-primary-dark px-1">digitalisation</span> de votre écurie, enfin <span className="underline">abordable</span></h2>
+        <p className="text-lg text-center w-3/4 mt-12">Offrez à votre écurie une gestion moderne et abordable grâce à nos écrans e-Ink robustes et personnalisables. La digitalisation ,n'a jamais été aussi accessible !</p>
+        <Link href="/tarif">
+              <div className="bg-primary-dark text-white rounded-md mt-10 text-center w-60 py-5 font-bold">
+                En savoir plus
+              </div>
+            </Link>
+      </div>
+        <Image src={hero}  alt="cheval au galop" fill={true} className="object-cover object-top"/>
+      </div>
+    </section>
     {/* qu'est ce que section */}
       <section className="py-14">
         <div className="flex">
@@ -187,7 +204,7 @@ const Solution = () => {
                 Prix raisonnable
               </li>
             </ul>
-            <div className="bg-white grow p-10 rounded-xl h-[420px] relative overflow-hidden">
+            <div className="bg-white grow p-10 rounded-3xl h-[420px] relative overflow-hidden">
               <h4 className="text-center text-xl font-bold">Ecran e-ink</h4>
               <div className="w-[520px] h-full absolute left-1/2 transform -translate-x-1/2 top-28 ">
                 <Image src={eInk} width={500} height={500} alt="illustration de l'écran e-ink" className="object-cover"/>
